ae2220adfb6a83740428bc4e974ed333501d7b09
Ravi Tej <ravitejk72007@gmail.com>
2017-09-19 07:49:36 -0400

    Changed messaging activity

--- app/src/main/java/com/example/a5280081/firebasecloudmessagingapp/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/a5280081/firebasecloudmessagingapp/MyFirebaseMessagingService.java
@@ -13,12 +13,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-        PendingIntent pendingIntent = PendingIntent.getActivity(this,0, intent, PendingIntent.FLAG_ONE_SHOT);
-
-
+        createNotification(remoteMessage);
     }
 
     public void createNotification(RemoteMessage remoteMsg){
