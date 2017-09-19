a2cdc247c927298661587f5edcd77de7ab2855bb
Ravi Tej <ravitejk72007@gmail.com>
2017-09-19 07:59:06 -0400

    Subscribed to pickuprequest

--- app/src/main/java/com/example/a5280081/firebasecloudmessagingapp/MainActivity.java
+++ app/src/main/java/com/example/a5280081/firebasecloudmessagingapp/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.a5280081.firebasecloudmessagingapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseMessaging.getInstance().subscribeToTopic("pickuprequest");
     }
 }
